dfedd3f5d5740e75502131cdc969430e5b523d23
Christopher Arroyo <carroyo11@lib5468-05.local>
2019-03-05 14:58:55 -0800

    Part2offlixster

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Christopher Arroyo on 3/5/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
